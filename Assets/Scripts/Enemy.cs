994e501b4998e18734d2968e0ce82dc7ef8bd0a0
Aldo <60137518+aldojack@users.noreply.github.com>
2020-08-26 12:09:21 +0100

    Fixed enemy land animation and included navmesh to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,38 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField]
-    private Transform player;
-    
+    private Animator enemyAnim;
+    private NavMeshAgent _navMesh;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player").GetComponent<Transform>();
+        enemyAnim = GetComponentInChildren<Animator>();
+        _navMesh = GetComponentInChildren<NavMeshAgent>();
+        _navMesh.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //EnemyFollows(); 
+        
     }
 
-    void EnemyFollows()
-    {
-        transform.LookAt(player);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ninja Stars"))
         {
+            Debug.Log("Hit with ninja star");
             Destroy(gameObject);
             Destroy(other.gameObject);
         }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            
+            enemyAnim.SetBool("Moving", false);
+            enemyAnim.SetBool("Grounded", true);
+            StartCoroutine(EnableNavMesh());
+            //Debug.Break();
+            //_navMesh.enabled = true;
+        }
+    }
 
+    IEnumerator EnableNavMesh()
+    {
+        yield return new WaitForSeconds(1f);
+        _navMesh.enabled = true;
     }
 }
